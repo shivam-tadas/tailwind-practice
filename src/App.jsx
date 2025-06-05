3212254683d79f0012f5a60b0135881b449b0d0e
shivam-tadas <shivam.tadas@outlook.in>
2025-06-05 18:54:31 +0530

    test responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <div class="grid grid-cols-3 gap-2 mt-2 mx-2">
-        <div class="h-16 w-16 rounded-full bg-blue-500" />
-        <div class="h-16 w-16 rounded-full bg-orange-500" />
-        <div class="h-16 w-16 rounded-full bg-green-500" />
+      <div class="sm:bg-amber-500 md:bg-amber-700">
+        <p class="text-black">Minimum width 768px</p>
       </div>
     </>
   )
