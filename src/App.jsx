2f2ef70b0c71f803c7a6c5b52de146e583c934d7
shivam-tadas <shivam.tadas@outlook.in>
2025-06-05 19:09:46 +0530

    implement dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 
 function App() {
+  var isDark = false
+
+  const toggleDarkMode = () => {
+    isDark = !isDark
+    document.body.classList.toggle('dark', isDark)
+  }
+
   return (
     <>
-      <div class="sm:bg-amber-500 md:bg-amber-700">
-        <p class="text-black">Minimum width 768px</p>
+      <div class="bg-white dark:bg-black text-black dark:text-white">
+        Dark mode test
       </div>
+      <button
+        id="toggleDark"
+        class="px-4 py-2 text-sm font-medium mt-8 ml-4 text-blue-900 bg-blue-100 rounded-md"
+        onClick={toggleDarkMode}
+      >
+        Toggle Dark Mode
+      </button>
     </>
   )
 }
