f0bf1e1c567e18e7b20f88dc03fda956a5c1891e
shivam-tadas <shivam.tadas@outlook.in>
2025-06-05 18:26:00 +0530

    Start with Hello world

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-xl text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
