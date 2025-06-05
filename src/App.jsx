4bae065daf95d0ad06e16c6578c4cdf762e8465a
shivam-tadas <shivam.tadas@outlook.in>
2025-06-05 18:47:29 +0530

    practice grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <div class="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-2 flex justify-center items-center">
-        <h1 class="text-center font-mono font-bold">
-          Hello world
-        </h1>
+      <div class="grid grid-cols-3 gap-2 mt-2 mx-2">
+        <div class="h-16 w-16 rounded-full bg-blue-500" />
+        <div class="h-16 w-16 rounded-full bg-orange-500" />
+        <div class="h-16 w-16 rounded-full bg-green-500" />
       </div>
     </>
   )
