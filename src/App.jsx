c48abfe4f620b4d18f57145dcb7ad183bfd26928
shivam-tadas <shivam.tadas@outlook.in>
2025-06-05 18:39:35 +0530

    improve Hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 class="text-xl text-center">
-        Hello world!
-      </h1>
+      <div class="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-2 flex justify-center items-center">
+        <h1 class="text-center font-mono font-bold">
+          Hello world
+        </h1>
+      </div>
     </>
   )
 }
